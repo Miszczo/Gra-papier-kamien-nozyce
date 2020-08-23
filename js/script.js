39dbb51262ee303cb0e058fc23fb017ea517292a
Michał Głowacki <mcglowack@gmail.com>
2020-08-23 14:37:28 +0200

    Add buttons in js and index.html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+
+
+
+var argButtonName, buttonScissors;
+
+/**
+ * Describe this function...
+ */
+function buttonClicked(argButtonName) {
+  clearMessages();
+  console.log(argButtonName + ' został kliknięty');
+}
+buttonScissors = document.getElementById('button-test');
+buttonScissors.addEventListener('click', function(){ buttonClicked('Guzik TEST'); });
+buttonScissors = document.getElementById('button-rock');
+buttonScissors.addEventListener('click', function(){ buttonClicked('kamień'); });
+buttonScissors = document.getElementById('button-paper');
+buttonScissors.addEventListener('click', function(){ buttonClicked('papier'); });
+buttonScissors = document.getElementById('button-scissors');
+buttonScissors.addEventListener('click', function(){ buttonClicked('nożyce'); });
+
 var argComputerMove, argMoveId, argPlayerMove, computerMove, playerInput, playerMove, randomNumber;
 
 /**
@@ -35,10 +56,7 @@ function displayResult(argPlayerMove, argComputerMove) {
   }
   printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 }
-playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('wybór ruchu gracza to: ' + playerInput);
-playerMove = getMoveName(playerInput);
-console.log('ruch gracza to: ' + playerMove);
+playerMove = argButtonName;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
 computerMove = getMoveName(randomNumber);
